05332fdec03a0b411ca860d48f94f62c29040704
Nolan Lawson <nolan.lawson@gmail.com>
2017-05-13 20:20:59 -0700

    more fixes

--- app/javascript/mastodon/sw-entry.js
+++ app/javascript/mastodon/sw-entry.js
@@ -12,13 +12,21 @@ const precacheUrls = [
 const signOutRegex = /^\/auth\/sign_out/;
 const emojiRegex = /^\/emoji\//
 
+const fetchOptions = {
+  credentials: 'include'
+};
+
 function getCache() {
   return caches.open(CACHE_KEY);
 }
 
 self.addEventListener('install', event => {
-  event.waitUntil(getCache().then(cache => {
-    return cache.addAll(precacheUrls);
+  event.waitUntil(getCache().then(function (cache) {
+    return Promise.all(precacheUrls.map(url => {
+      return fetch(url, fetchOptions).then(response => {
+        return cache.put(new Request(response.url), response);
+      });
+    }));
   }));
 });
 
@@ -34,9 +42,20 @@ self.addEventListener('fetch', event => {
         cache.delete(fullUrl);
       });
     });
-  } else if (method === 'GET' && emojiRegex.test(url.pathname)) {
+  } else if (method === 'GET' && emojiRegex.test(new URL(url).pathname)) {
     event.respondWith(
-      caches.match(event.request).then(r => r || fetch(event.request))
+      getCache().then(cache => {
+        return cache.match(event.request).then(cacheResponse => {
+          if (cacheResponse) {
+            return cacheResponse;
+          }
+          return fetch(event.request, fetchOptions).then(fetchResponse => {
+            // cache as a side effect, not meant to block response
+            cache.put(event.request, fetchResponse);
+            return fetchResponse;
+          });
+        });
+      })
     );
   }
 });
