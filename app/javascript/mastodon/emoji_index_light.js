c7832320ab57d26d44bda8d3e2e6e6d2d50a1f50
Nolan Lawson <nolan@nolanlawson.com>
2017-09-23 17:21:52 -0700

    fix encoding

--- app/javascript/mastodon/emoji_index_light.js
+++ app/javascript/mastodon/emoji_index_light.js
@@ -1,5 +1,10 @@
 // @preval
 // Force tree shaking on emoji-mart by exposing just a subset of its functionality
 
-const emojiMart = require('emoji-mart');
-module.exports.emojiIndex = JSON.parse(JSON.stringify(emojiMart.emojiIndex));
+const emojiIndex = require('emoji-mart').emojiIndex;
+
+// This fixes some encoding issues with preval
+emojiIndex.emojis = JSON.parse(JSON.stringify(emojiIndex.emojis));
+emojiIndex.emoticons = JSON.parse(JSON.stringify(emojiIndex.emoticons));
+
+module.exports.emojiIndex = emojiIndex;
