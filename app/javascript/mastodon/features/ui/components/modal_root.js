0c9465e270b59f3e7f852721a3737ff706d7e7f5
Nolan Lawson <nolan@nolanlawson.com>
2017-09-27 21:48:01 -0700

    fix initial flicker

--- app/javascript/mastodon/features/ui/components/modal_root.js
+++ app/javascript/mastodon/features/ui/components/modal_root.js
@@ -98,9 +98,13 @@ export default class ModalRoot extends React.PureComponent {
         <div style={{ pointerEvents: visible ? 'auto' : 'none' }}>
           <div role='presentation' className='modal-root__overlay' onClick={onClose} />
           <div role='dialog' className='modal-root__container'>
-            <BundleContainer fetchComponent={MODAL_COMPONENTS[type]} loading={this.renderLoading(type)} error={this.renderError} renderDelay={200}>
-              {(SpecificComponent) => <SpecificComponent {...props} onClose={onClose} />}
-            </BundleContainer>
+            {
+              visible ?
+                (<BundleContainer fetchComponent={MODAL_COMPONENTS[type]} loading={this.renderLoading(type)} error={this.renderError} renderDelay={200}>
+                  {(SpecificComponent) => <SpecificComponent {...props} onClose={onClose} />}
+                </BundleContainer>) :
+              null
+            }
           </div>
         </div>
       </div>
